381a6c73781997b97e1fb812fdb22266f1559f3b
ducnv <ducnv@mirabo-global.com>
2026-01-10 13:05:28 +0700

    update code

--- api/src/super-admin/super-admin.controller.ts
+++ api/src/super-admin/super-admin.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Post, Param, UseGuards } from '@nestjs/common';
 import { SuperAdminService } from './super-admin.service';
-import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
-import { RolesGuard } from '@/auth/guards/roles.guard';
-import { Roles } from '@/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import { PermissionsGuard } from '@/auth/permissions.guard';
+import { Permissions } from '@/auth/decorators/permissions.decorator';
 
 @Controller('super-admin')
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('SUPER_ADMIN')
+@UseGuards(JwtAuthGuard, PermissionsGuard)
+@Permissions('SUPER_ADMIN')
 export class SuperAdminController {
   constructor(private readonly service: SuperAdminService) {}
 
